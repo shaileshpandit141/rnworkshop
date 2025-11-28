17f0c91947b518c44eb6c191dba446aff862c221
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-11-28 22:58:38 +0530

    feat: configure safe area view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { SafeAreaView } from "@/components/ui";
 import useColors from '@/hooks/useColors';
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
@@ -5,60 +6,61 @@ import React from 'react';
 
 export default function TabLayout() {
   const colors = useColors();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: colors.brand.primary,
-        headerShown: true,
-        headerTitleAlign: "center",
-        headerShadowVisible: false,
-        animation: "shift",
-        headerTitleStyle: {
-          textTransform: "capitalize",
-        },
-        tabBarLabelStyle: {
-          textTransform: "capitalize",
-        },
-        headerStyle: {
-          height: 80,
-          backgroundColor: colors.core.header,
-          borderBottomWidth: .5,
-          borderBottomColor: colors.core.border,
-        },
-        tabBarStyle: {
-          elevation: 0,
-          shadowOpacity: 0,
-          backgroundColor: colors.core.tab,
-          borderBottomWidth: 0,
-          borderTopWidth: .5,
-          borderTopColor: colors.core.border,
-        },
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "screens",
-          tabBarLabel: "screens",
-          tabBarIcon: ({ focused, color, size }) => (
-            focused
-              ? <Ionicons name="link-outline" size={size} color={color} />
-              : <Ionicons name="link" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "settings",
-          tabBarLabel: "settings",
-          tabBarIcon: ({ focused, color, size }) => (
-            focused
-              ? <Ionicons name="settings" size={size} color={color} />
-              : <Ionicons name="settings-outline" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <SafeAreaView>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: colors.brand.primary,
+          headerShown: true,
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+          animation: "shift",
+          headerTitleStyle: {
+            textTransform: "capitalize",
+          },
+          tabBarLabelStyle: {
+            textTransform: "capitalize",
+          },
+          headerStyle: {
+            height: 80,
+            backgroundColor: colors.core.header,
+            borderBottomWidth: .5,
+            borderBottomColor: colors.core.border,
+          },
+          tabBarStyle: {
+            elevation: 0,
+            shadowOpacity: 0,
+            backgroundColor: colors.core.tab,
+            borderBottomWidth: 0,
+            borderTopWidth: .5,
+            borderTopColor: colors.core.border,
+          },
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "screens",
+            tabBarLabel: "screens",
+            tabBarIcon: ({ focused, color, size }) => (
+              focused
+                ? <Ionicons name="link-outline" size={size} color={color} />
+                : <Ionicons name="link" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "settings",
+            tabBarLabel: "settings",
+            tabBarIcon: ({ focused, color, size }) => (
+              focused
+                ? <Ionicons name="settings" size={size} color={color} />
+                : <Ionicons name="settings-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -13,12 +14,14 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </ThemeProvider>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
